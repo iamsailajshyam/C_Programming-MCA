ba8ced0342fd47ae8dc1f7391d7c36e9927318da
SAILAJ SHYAM_MCA_A_2024-26_41_UEMK <139752636+iamsailajshyam@users.noreply.github.com>
2025-01-18 15:06:01 +0530

    Create 1.c

---
+++ Assignment 2/1.c
@@ -0,0 +1,37 @@
+//C Program for Arithmetic Operations using User-Defined Functions:**
+
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+float divide(int a, int b) {
+    if (b != 0) {
+        return (float)a / b;
+    } else {
+        printf("Division by zero error!\n");
+        return 0;
+    }
+}
+
+int main() {
+    int num1, num2;
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("Addition: %d\n", add(num1, num2));
+    printf("Subtraction: %d\n", subtract(num1, num2));
+    printf("Multiplication: %d\n", multiply(num1, num2));
+    printf("Division: %.2f\n", divide(num1, num2));
+
+    return 0;
+}
