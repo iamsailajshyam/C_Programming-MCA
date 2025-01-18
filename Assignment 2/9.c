b0248e1e5789b2c707fcf48654b12f64a4083cfd
SAILAJ SHYAM_MCA_A_2024-26_41_UEMK <139752636+iamsailajshyam@users.noreply.github.com>
2025-01-18 15:08:56 +0530

    Update 9.c

--- Assignment 2/9.c
+++ Assignment 2/9.c
@@ -8,3 +8,41 @@ int isPrime(int n) {
     }
     return 1;
 }
+
+int isArmstrong(int n) {
+    int sum = 0, temp = n, digits = 0;
+    while (temp) {
+        digits++;
+        temp /= 10;
+    }
+    temp = n;
+    while (temp) {
+        sum += pow(temp % 10, digits);
+        temp /= 10;
+    }
+    return sum == n;
+}
+
+int isPerfect(int n) {
+    int sum = 0;
+    for (int i = 1; i < n; i++) {
+        if (n % i == 0)
+            sum += i;
+    }
+    return sum == n;
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isPrime(num))
+        printf("%d is a Prime number\n", num);
+    if (isArmstrong(num))
+        printf("%d is an Armstrong number\n", num);
+    if (isPerfect(num))
+        printf("%d is a Perfect number\n", num);
+
+    return 0;
+}
